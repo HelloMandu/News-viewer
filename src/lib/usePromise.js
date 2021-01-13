bef3696a972ed1239b7ab1f58756f3e1a5cbafbb
HelloMandu <tjdals6695@gmail.com>
2021-01-13 18:55:49 +0900

    first commit

---
+++ src/lib/usePromise.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+export default function usePromise(promiseCreator, deps) {
+  const [loading, setLoading] = useState(false);
+  const [resolved, setResolved] = useState(null);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const resolved = await promiseCreator();
+        setResolved(resolved);
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps);
+  return [loading, resolved, error];
+}
