01ddc41ee5c3f8b8615a5a4cd7012f0e1781e03b
HelloMandu <tjdals6695@gmail.com>
2021-01-13 18:58:52 +0900

    Updates

---
+++ precache-manifest.74dca2c7b5559bae4b7837234521e8e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d01e54c8a0c13c85547346a7585ba73",
+    "url": "/News-viewer/index.html"
+  },
+  {
+    "revision": "50fca876afff9609ae1f",
+    "url": "/News-viewer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2a1b688ab6b364c24542",
+    "url": "/News-viewer/static/js/2.56f9d7b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/News-viewer/static/js/2.56f9d7b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50fca876afff9609ae1f",
+    "url": "/News-viewer/static/js/main.e1b0da5f.chunk.js"
+  },
+  {
+    "revision": "e6a958cdbc6085a74242",
+    "url": "/News-viewer/static/js/runtime-main.c9c4d37d.js"
+  }
+]);
